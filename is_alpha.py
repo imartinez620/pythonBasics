1a2e1c1b41f673cd693e5805dc7a9e0426077929
imartinez620 <info.imartinez@gmail.com>
2024-02-15 16:56:37 +0100

    partition & is_alpha

---
+++ is_alpha.py
@@ -0,0 +1,8 @@
+number = '5'
+name = 'Carlos'
+
+print(number.isalpha())
+print(name.isalpha())
+
+print(number.isnumeric())
+print(name.isnumeric())
---
+++ partition_function.py
@@ -0,0 +1,14 @@
+heading = 'Python: An Introduction'
+
+header, _, subheader = heading.partition(": ")
+
+print(header)
+print(subheader)
+
+tags = 'camion,coche,moto'
+list_of_tags = tags.split(',')
+
+print(list_of_tags)
+
+list_of_heading = heading.split(': ')
+print(list_of_heading)
