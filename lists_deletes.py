53831df9c02e6ca61f4d515134fe983ebde40bd3
imartinez620 <info.imartinez@gmail.com>
2024-02-19 18:55:50 +0100

    more exersices

---
+++ lists_deletes.py
@@ -0,0 +1,12 @@
+users = ['Kristine','Tiffany','Jordan', 'Charles']
+print(users)
+
+users.remove('Jordan')
+print(users)
+
+popped_user = users.pop()
+print(popped_user)
+print(users)
+
+del users[0]
+print(users)
