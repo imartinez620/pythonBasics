dea0d56797fb60055201dd90fba1f6a0d6478932
imartinez620 <info.imartinez@gmail.com>
2024-02-21 17:32:59 +0100

    list and nested dictionaries

---
+++ list_and_dictionaries.py
@@ -0,0 +1,20 @@
+teams = [
+	{
+		'astros':{
+			'2B': 'Altuve',
+			'SS': 'Correa',
+			'3B': 'Bregman'
+		}
+	},
+	{
+		'angels':{
+			'OF': 'Trout',
+			'DH': 'Pujols'
+		}
+	}
+]
+
+print(teams[0])
+
+angels = teams[1].get('angels','Team not found')
+print(list(angels.values())[1])
