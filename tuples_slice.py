77d617a7988180d8558e11c30b9a5c73304e15df
imartinez620 <info.imartinez@gmail.com>
2024-02-21 18:54:42 +0100

    tuples removing

--- tuples_slice.py
+++ tuples_slice.py
@@ -1,6 +1,22 @@
-post = ('Python Basiscs', 'Intro Guide to Python', 'Some cool python content','published')
+post = ('Python Basiscs', 'Intro Guide to Python', 'Some cool python content','published',
+	'other_element')
 
 print(post[:2])
 print(post[2:])
 print(post[1:3])
 print(post[1::2])
+
+#Removing element from end
+post = post[:-1]
+
+#Removing element from beginning
+post = post[1:]
+print(post)
+
+#Removing specific element (messy/not recommended)
+post = list(post)
+post.remove('Some cool python content')
+post = tuple(post)
+
+print(post)
+
