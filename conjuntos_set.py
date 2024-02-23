98493f6507b41cbe46daa3c7b2dbae0ddb060a17
imartinez620 <info.imartinez@gmail.com>
2024-02-23 12:57:16 +0100

    conjuntos set

---
+++ conjuntos_set.py
@@ -0,0 +1,17 @@
+tags = {
+	'python',
+	'coding',
+	'tutorials',
+	'coding'
+}
+
+print(tags)
+
+#error
+#print(tags[0])
+
+query_one = 'python' in tags
+print(query_one)
+
+query_two = 'java' in tags
+print(query_two)
