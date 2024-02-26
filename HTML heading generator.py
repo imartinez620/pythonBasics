104a49863720304c3a93720b8d5560ee5920e47d
imartinez620 <info.imartinez@gmail.com>
2024-02-26 15:44:11 +0100

    string interpolation

---
+++ HTML heading generator.py
@@ -0,0 +1,20 @@
+"""
+heading_generator(title, heading_type)
+
+heading_generator('Greeting', '1')
+<h1>Greeting<\h1>
+heading_generator('Hi there', '3')
+<h3>Hi there<\h3>
+"""
+
+def heading_generator(title, heading_type):
+	print(f"<h{heading_type}>{title}<\h{heading_type}>")
+
+heading_generator('Greeting', '1')
+heading_generator('Hi there', '3')
+
+#solution
+def heading_generator(title, heading_type):
+	return("<h{0}>".format(heading_type) +f"{title}" + 
+		"<\h{0}>".format(heading_type))
+
