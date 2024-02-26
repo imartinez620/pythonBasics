a0bf741a9a749e908e151d601cf677033f5f74ff
imartinez620 <info.imartinez@gmail.com>
2024-02-26 17:28:31 +0100

    loops while

--- while.py
+++ while.py
@@ -1,1 +1,22 @@
-while
+nums = list(range(1,100))
+
+#print(nums)
+
+#for num in nums:
+#	print(num)
+
+while len(nums) > 0:
+	print(nums.pop())
+
+def guessing_game():
+	while True:
+		print('What is your guess?')
+		guess = input()
+
+		if guess == '42':
+			print('You correctly guessed it!')
+			return False
+		else:
+			print(f'No, {guess} is not the answer, please try again\n')
+
+guessing_game()
