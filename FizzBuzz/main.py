9da9f03f91107935ca1d488694245e5a88b1c696
imartinez620 <info.imartinez@gmail.com>
2024-02-27 15:40:33 +0100

    FizzBuzz Project

---
+++ FizzBuzz/main.py
@@ -0,0 +1,27 @@
+"""
+Write a program that prints the numers from 1 to 'n'.
+But for multiples of three print 'Fizz' instead of the number
+and for the multiples of five print 'Buzz'.
+For numbers which are multiples of both three and five print
+'FizzBuzz'.
+
+Use:
+- Function
+- Looping
+- Conditionals
+- Math operator
+"""
+
+def fizz_buzz(max_number):
+	for num in range(1, max_number + 1):
+		if num % 3 == 0 and num % 5 == 0:
+			print('FizzBuzz')
+		elif num % 3 == 0:
+			print('Fizz')
+		elif num % 5 == 0:
+			print('Buzz')
+		else:
+			print(num) 	
+		
+
+fizz_buzz(100)
---
+++ funciones_landa.py
@@ -0,0 +1,9 @@
+full_name = lambda first_name, last_name: f'{first_name} {last_name}'
+
+print(full_name('Peter', 'Parker'))
+
+def greting(name):
+	print(f'Hi there {name}')
+
+
+greting(full_name('Bruce', 'Wayne'))
