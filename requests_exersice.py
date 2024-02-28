c2d7e9e0a55285fb0f5271f66438cd4733b36784
imartinez620 <info.imartinez@gmail.com>
2024-02-28 17:15:36 +0100

    request project

--- requests_exersice.py
+++ requests_exersice.py
@@ -5,10 +5,12 @@ r = requests.get('https://api.dailysmarty.com/posts')
 
 r_list = r.json()['posts']
 #pprint.pprint(r_list)
+#r.json()
+#pprint.pprint(r.json()['posts'][0])
+#pprint.pprint(r.json()['posts'][0]['url_for_post'])
 
 for url in r_list:
 	if url['url_for_post']:
 		print(url['url_for_post'])
-#r.json()
-#pprint.pprint(r.json()['posts'][0])
-#pprint.pprint(r.json()['posts'][0]['url_for_post'])
+
+
--- requests_project/main.py
+++ requests_project/main.py
@@ -1,19 +1,47 @@
 import requests
 import inflection
+from bs4 import BeautifulSoup
 
+#get data from the site
 r = requests.get('http://www.dailysmarty.com/topics/python')
+centinel = 'data-turbolinks="false" href="' 
+enlaces_manual = []
+enlaces_with_beauti = []
 
-#print(r.text)
 
-tx = "/posts/steps-for-building-a-flask-api-application-with-python-3".lstrip("/popts/")
-print(tx)
-tx = tx.split('-')
-print(tx)
+def get_links_list():
+	start_index = 0
+	final_index = 0
+	max_value_index = r.text.rfind(centinel)
+	
+	while start_index < max_value_index:	
+		start_index = r.text[final_index:].find(centinel) + final_index
+		final_index = start_index + r.text[start_index:].index('">')
+		enlace = r.text[start_index:final_index].lstrip(centinel).split('/')[-1]	
+		enlaces_manual.append(enlace)
+	
+	#print(enlaces_manual)
 
-tx_2 = ""
-for noun in tx:
-	tx_2 += ' ' + noun
-print(tx_2)
 
-print(inflection.camelize(tx_2, uppercase_first_letter=True))
-print(tx_2)
+def get_links_list_with_beautifulshop():
+	soup = BeautifulSoup(r.text, 'html.parser')
+	links_wrapper = soup.find_all("h2")
+
+	for a_link in links_wrapper:
+		str_link = str(a_link).partition('">')[0]
+		str_link = str(str_link).split('/')[-1]
+		enlaces_with_beauti.append(str_link)
+		
+	#print(enlaces_with_beauti)
+
+
+def prepare_link_list(links):
+	for link in links:
+		print(inflection.titleize(link))
+
+
+#main
+#get_links_list()
+#prepare_link_list(enlaces_manual)
+get_links_list_with_beautifulshop()
+prepare_link_list(enlaces_with_beauti)
